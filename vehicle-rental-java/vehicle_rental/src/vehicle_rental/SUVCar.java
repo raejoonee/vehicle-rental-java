f74b37d440420b61e6bba6c21981fffb50e7c2de
김성준 <ksjune723@naverlcom>
2020-08-07 09:41:33 +0900

    Implemented Car Class

--- vehicle-rental-java/vehicle_rental/src/vehicle_rental/SUVCar.java
+++ vehicle-rental-java/vehicle_rental/src/vehicle_rental/SUVCar.java
@@ -6,10 +6,17 @@ public class SUVCar extends Car {
 		
 	}
 	public SUVCar(String numberplate) {
-		super();
+		super(numberplate);
 		initialFee = 20000;
 		additionalFee = 180;
 		discountRatio = 0.1;
 	}
+	@Override
+	public String toString() {
+		return "SUV: [numberPlate=" + numberPlate + ", initialFee=" + initialFee + ", additionalFee=" + additionalFee
+				+ ", discountRatio=" + discountRatio + "]";
+	}
+	
+	
 	
 }
