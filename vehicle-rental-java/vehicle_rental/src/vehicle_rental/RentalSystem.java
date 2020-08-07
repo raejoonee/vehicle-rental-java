cc0197fc781970edf7b4ae0e5b1f5acff4fc7efa
guapxcv <44992640+guapxcv@users.noreply.github.com>
2020-08-07 15:56:48 +0900

    Update RentalSystem.java

--- vehicle-rental-java/vehicle_rental/src/vehicle_rental/RentalSystem.java
+++ vehicle-rental-java/vehicle_rental/src/vehicle_rental/RentalSystem.java
@@ -1,13 +1,11 @@
 package vehicle_rental;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class RentalSystem {
-
+  
 	static final String carListPath = "carList.csv";
 	public SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 	
@@ -17,8 +15,10 @@ public class RentalSystem {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public static HashSet<Car> cm = new HashSet<>(); // 빌릴수 있는 차량들
+	
 	public void showList() {
-		Iterator<Car> iterator = CarManager.cm.iterator();
+		Iterator<Car> iterator = cm.iterator();
 		int count = 1;
 		while(iterator.hasNext()) {
 			System.out.printf("%03d ", count++);
@@ -29,30 +29,49 @@ public class RentalSystem {
 	public void registerCar(Car car) throws IOException {
 		String now = sdf.format(new Date());
 		try {
-	        FileWriter fw = new FileWriter(carListPath, true);
-	       
+	        FileWriter fw = new FileWriter(new File(carListPath), true);
 	        if (car instanceof SUVCar){
-	        	System.out.println("asdjflksadjf");
-	        	fw.write("\nSUV, " + car.numberPlate + ", " + now + ", false");
-	        	CarManager.cm.add(car);
+	        	fw.write("SUV, " + car.numberPlate + ", " + now + ", FALSE");
 	        } else if (car instanceof CompactCar){
-	        	System.out.println("asdklfjksladfj");
-	        	fw.write("\ncompactCar, " + car.numberPlate + ", " + now + ", false");
-	        	CarManager.cm.add(car);
+	        	fw.write("compactCar, " + car.numberPlate + ", " + now + ", FALSE");
 	        } else {
-	        	fw.write("\npassengerCar, " + car.numberPlate + ", " + now + ", false");
-	        	CarManager.cm.add(car);
+	        	fw.write("passengerCar, " + car.numberPlate + ", " + now + ", FALSE");
 	        }
-	        fw.flush();
 	        fw.close();
 	    } catch (IOException e) {
 	        e.printStackTrace();
 	    }
 	}
 	
+	public String deleteCar(int position) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(carListPath))));
+		String dummy = "";
+		String line = "";
+		for(int i = 0 ; i < position; i++) {
+			line = br.readLine();
+			dummy += (line +"\r\n");
+		}
+		
+		String delData = br.readLine();
+		//Log.d("mstag","삭제되는 데이터 = "+delData);
+		
+		while((line = br.readLine())!=null) {
+			dummy += (line +"\r\n");
+		}
+		FileWriter fw = new FileWriter(carListPath);
+
+		fw.write(dummy);
+		fw.flush();
+		fw.close();
+		br.close();
+		return delData;
+	}
+	
+
 	public static RentalSystem getInstance() {
 		if(instance == null) instance = new RentalSystem();
 		return instance;
 	}
-
+	
+	
 }
